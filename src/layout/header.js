84576557d60ac4ae304503e1f5bf409043a2792b
Bikram Nath <bikram.1795@gmail.com>
2021-01-26 18:32:16 +0530

    changes

--- src/layout/header.js
+++ src/layout/header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
     render() {
         return (
             <div>
-                Lyrics Finder
+                Lyrics HUnter
             </div>
         )
     }
