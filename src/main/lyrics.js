65bf8c8c2fe9e42921dcd705bac30ceedd4d91ce
Bikram Nath <bikram.1795@gmail.com>
2021-02-09 11:42:23 +0530

    netlify warning

--- src/main/lyrics.js
+++ src/main/lyrics.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from "axios";
-import AutoComplete from "./autoComplete";
 export default class Lyrics extends Component {
   state = {
     song: null,
